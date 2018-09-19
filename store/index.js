90e80d9a93aa59e1a99b8fe7236a17f5b024053f
Javier <javieerrubio11@gmail.com>
2018-09-19 19:39:07 +0200

    New icons added in navigation drawer

--- store/index.js
+++ store/index.js
@@ -1,10 +1,10 @@
 export const state = () => ({
   sections: [
-    { anchor: 'section-1', name: 'Main', color: 'teal', icon: 'add' },
-    { anchor: 'section-2', name: 'Certification', color: 'green', icon: 'add' },
-    { anchor: 'section-3', name: 'Experiencie', color: 'blue', icon: 'add' },
-    { anchor: 'section-4', name: 'Skill', color: 'green', icon: 'add' },
-    { anchor: 'section-5', name: 'Proyect', color: 'amber', icon: 'add' },
-    { anchor: 'section-6', name: 'Github', color: 'lime', icon: 'add' },
+    { anchor: 'section-1', name: 'Main', color: 'teal', icon: 'home' },
+    { anchor: 'section-2', name: 'Certification', color: 'green', icon: 'book' },
+    { anchor: 'section-3', name: 'Experiencie', color: 'blue', icon: 'work' },
+    { anchor: 'section-4', name: 'Skill', color: 'green', icon: 'filter_5' },
+    { anchor: 'section-5', name: 'Proyect', color: 'amber', icon: 'dashboard' },
+    { anchor: 'section-6', name: 'Github', color: 'lime', icon: 'mdi-github-circle' },
   ]
 })
