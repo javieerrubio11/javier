ecbc1013ee8a353fe5cf15fb37fa99e7c48fa9ad
Javier <javieerrubio11@gmail.com>
2018-10-27 16:21:05 +0200

    Update dependencies

---
+++ components/Blog.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <h2 class="display-2 pb-4">Blog</h2>
+
+  </v-container>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+
+  data() {
+    return {
+      post: {},
+    }
+  },
+
+  mounted() {
+    this.loadData()
+  },
+
+  methods: {
+    async loadData() {
+      let data = await axios.get('http://tecnoxperiencia.com/wp-json/wp/v2/posts?author=1')
+      console.log(data)
+    },
+  },
+
+}
+</script>
+
+<style scope>
+
+</style>
