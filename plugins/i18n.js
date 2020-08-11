80d371a76c092a01587bc942ddc89b511f0ff0ce
Javier <javieerrubio11@gmail.com>
2020-08-11 20:07:22 +0200

    Update language traduction

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -5,7 +5,6 @@ Vue.use(VueI18n)
 
 export default ({ app, store }) => {
   // Set i18n instance on app
-  // This way we can use it in middleware and pages asyncData/fetch
   app.i18n = new VueI18n({
     locale: store.state.locale,
     fallbackLocale: 'en',
@@ -14,12 +13,4 @@ export default ({ app, store }) => {
       'es': require('~/locales/es.json')
     }
   })
-
-  app.i18n.path = (link) => {
-    if (app.i18n.locale === app.i18n.fallbackLocale) {
-      return `/${link}`
-    }
-
-    return `/${app.i18n.locale}/${link}`
-  }
 }
